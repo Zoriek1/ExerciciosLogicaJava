116ce1c840e7d9a8dfc59d4a3138352351d6c514
Zoriek <caionixt@gmail.com>
2025-04-04 17:47:51 -0300

    Commit 4, Exercicio 8 resolvido

--- src/ExerciciosJava/Exercicio8/exercicio8.java
+++ src/ExerciciosJava/Exercicio8/exercicio8.java
@@ -3,10 +3,24 @@ package ExerciciosJava.Exercicio8;
 // 8 - Faça um algoritmo que leia três valores inteiros diferentes e imprima na tela os valores em ordem decrescente.
 //
 
+import java.util.Arrays;
+import java.util.Collections;
+
 public class exercicio8 {
 
-    public static void main(String[] args) {
+    public static void main(String[] args)  {
 
+        int A = 3;
+        int B = 16;
+        int C = 9;
 
+        // Criar um array com os números passados.
+        int[] numeros = {A,B,C};
+
+        Arrays.sort(numeros);
+
+        for (int i = numeros.length - 1; i >= 0; i--) {
+            System.out.print(numeros[i] + ", ");
+        }
     }
 }
---
+++ src/ExerciciosJava/Exercicio9/exercicio9.java
@@ -0,0 +1,7 @@
+package ExerciciosJava.Exercicio9;
+
+// 9 - Faça um algoritmo que calcule o IMC (Índice de Massa Corporal) de uma pessoa, leia o seu peso e sua altura e imprima na tela sua condição
+//
+
+public class exercicio9 {
+}
